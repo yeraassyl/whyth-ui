d12ecb98a8a78c46ff1b9a3b5758c3ca6cfb0d57
yeraassyl <yerassyl88@gmail.com>
2023-04-15 23:58:12 +0600

    github actions workflow

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         ['/session', '/lesson', '/prompt', '/chat-history'],
         createProxyMiddleware({
-            target: 'http://localhost:1337', // Replace with the address of your backend server
+            target: 'http://64.226.105.245:1337', // Replace with the address of your backend server
             changeOrigin: true,
         })
     );
