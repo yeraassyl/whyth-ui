ff830fb865855a33c655f6b43a2d81091aa1cf4f
yeraassyl <yerassyl88@gmail.com>
2023-04-16 16:16:49 +0600

    update backend ip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         ['/session', '/lesson', '/prompt', '/chat-history'],
         createProxyMiddleware({
-            target: 'http://64.226.105.245:1337', // Replace with the address of your backend server
+            target: 'http://142.93.97.228:1337', // Replace with the address of your backend server
             changeOrigin: true,
         })
     );
