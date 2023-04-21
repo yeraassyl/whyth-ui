62e2825b8ba887eeedb0aba62c24bff241a53c2b
yeraassyl <yerassyl88@gmail.com>
2023-04-21 19:02:56 +0600

    remove proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(
-        ['/api/session', '/api/lesson', '/api/prompt', '/api/chat-history'],
-        createProxyMiddleware({
-            target: 'http://localhost:1337', // Replace with the address of your backend server
-            changeOrigin: true,
-        })
-    );
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+//
+// module.exports = function (app) {
+//     app.use(
+//         ['/api/session', '/api/lesson', '/api/prompt', '/api/chat-history'],
+//         createProxyMiddleware({
+//             target: 'http://localhost:1337', // Replace with the address of your backend server
+//             changeOrigin: true,
+//         })
+//     );
+// };
