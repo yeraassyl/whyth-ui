25fd6f7fe3c75b78f6e5c0726ab5a2678e12d934
yeraassyl <yerassyl88@gmail.com>
2023-04-29 18:21:50 +0600

    uncommented setupProxy.js & update ci/cd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,12 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-//
-// module.exports = function (app) {
-//     app.use(
-//         ['/api/session', '/api/lesson', '/api/prompt', '/api/chat-history'],
-//         createProxyMiddleware({
-//             target: 'http://localhost:1337', // Replace with the address of your backend server
-//             changeOrigin: true,
-//         })
-//     );
-// };
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// For development only
+// On production, use nginx to proxy requests to the backend server
+module.exports = function (app) {
+    app.use(
+        ['/api/session', '/api/lesson', '/api/prompt', '/api/chat-history'],
+        createProxyMiddleware({
+            target: 'http://localhost:1337', // Replace with the address of your backend server
+            changeOrigin: true,
+        })
+    );
+};
